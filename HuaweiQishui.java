456d5145b5e39ea16435800d5405a9bafd0d7c4a
duxiaofei <ontway@163.com>
2016-03-22 21:57:45 +0800

    Create HuaweiQishui.java
    “某商店规定：三个空汽水瓶可以换一瓶汽水。小张手上有十个空汽水瓶，她最多可以换多少瓶汽水喝？”答案是5瓶，方法如下：先用9个空瓶子换3瓶汽水，喝掉3瓶满的，喝完以后4个空瓶子，用3个再换一瓶，喝掉这瓶满的，这时候剩2个空瓶子。然后你让老板先借给你一瓶汽水，喝掉这瓶满的，喝完以后用3个空瓶子换一瓶满的还给老板。如果小张手上有n个空汽水瓶，最多可以换多少瓶汽水喝？

---
+++ HuaweiQishui.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class HuaweiQishui{
+	   
+	public static int numberOfWater(int voidNum){
+	   
+	   if(voidNum<=1) return 0;
+       if(voidNum==2) return 1;
+       else return (voidNum/3)+numberOfWater(voidNum/3+voidNum%3);
+	}
+	
+	public static void main(String args[]){
+		 Scanner input=new Scanner(System.in);
+		 int[] nums=new int[100];
+		 int i=0;
+		do{
+		 
+			 nums[i]=input.nextInt();
+			 i++;
+		 }while(nums[i-1]!=0&&i<nums.length);
+		//System.out.println();
+		 input.close();
+		 int j=0;
+		 while(nums[j]!=0){
+			 
+			 System.out.println(numberOfWater(nums[j]));
+			 j++;
+		 }
+		 
+	}
+}
