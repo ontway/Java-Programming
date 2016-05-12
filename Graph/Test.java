f4dd22d71ced3b6ec0b0c36f19777c89a599663b
ontway <ontway@163.com>
2016-05-12 11:31:35 +0800

    add TestClass

---
+++ Graph/Test.java
@@ -0,0 +1,13 @@
+package advance;
+public class Test {
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           
+           Test t=new Test();
+           int d=t.getC();
+           System.out.println(d);
+           
+	}
+
+}
